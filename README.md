92bee733be23ac19e1a7ce6d16d133a11f0cecbf
paulotunis <paulotunis@hotmail.com>
2021-05-15 17:07:15 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,14 @@
+# API Rest de consulta de cidades do Brasil
+
+Projeto de desenvolvimento uma API Rest de consulta de cidades do Brasil com dados comparativos para o cálculo de distância entre cidades.
+
+Desenvolvido durante Bootcamp oferecido pela [**Digital Innovation One**](https://web.digitalinnovation.one/home) e ministrado pelo professor [**André Gomes**](https://www.linkedin.com/in/andreluisgomes/).
+
+
+
+**Foram utilizados:** 
+
+- Java;
+- Spring e 
+- Postgres.
+
---
+++ cities-api/src/main/java/com/github/paulotunis/citiesapi/countries/CountryResource.java
@@ -0,0 +1,23 @@
+package com.github.paulotunis.countries.resources;
+
+import com.github.paulotunis.countries.entities.Country;
+import com.github.paulotunis.countries.repositories.CountryRepository;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CountryResource {
+
+    private final CountryRepository repository;
+
+    public CountryResource(final CountryRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping("/countries")
+    public List<Country> cities() {
+
+        return repository.findAll();
+    }
+}
